ffac2d53bbbeeb0373ed7a712454eddf08c12cd8
dadakhon09 <odadaxon27@mail.ru>
2020-02-07 22:13:42 +0500

    ...

--- app/medicine/urls.py
+++ app/medicine/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from app.medicine.views import MedicineList, MedicineDetail, MedicineCreate
+from app.medicine.views import MedicineList, MedicineDetail, MedicineCreate, MedicineListByPharmacy
 
 urlpatterns = [
     path('list/', MedicineList.as_view(), name='medicine-list'),
     path('create/', MedicineCreate.as_view(), name='medicine-create'),
     path('list/<int:id>/', MedicineDetail.as_view(), name='medicine-detail'),
+    path('list/pharmacy/<int:pharmacy_id>/', MedicineListByPharmacy.as_view(), name='medicine-list-by-pharmacy'),
 ]
--- app/pharmacy/views.py
+++ app/pharmacy/views.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
 
-from app.models import Pharmacy, MedicinePharmacyRel
+from app.models import Pharmacy, MedicinePharmacyRel, Medicine
 from app.pharmacy.serializers import PharmacySerializer, MedicinePharmacyRelSerializer
 
 
@@ -38,3 +38,14 @@ class MedicinePharmacyRelDetail(RetrieveUpdateDestroyAPIView):
     model = MedicinePharmacyRel
     queryset = MedicinePharmacyRel.objects.all()
     serializer_class = MedicinePharmacyRelSerializer
+
+
+class PharmacyListByMedicine(ListAPIView):
+    lookup_field = 'id'
+    model = Pharmacy
+    serializer_class = PharmacySerializer
+
+    def get_queryset(self):
+        medicine_id = self.kwargs['medicine_id']
+        medicine = Medicine.objects.get(id=medicine_id)
+        return MedicinePharmacyRel.objects.filter(medicine=medicine)
