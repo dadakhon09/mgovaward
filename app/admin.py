6f6fcff1db8d0ab175effe6fd7d9be2ed2c284b3
dadakhon09 <odadaxon27@mail.ru>
2020-02-09 17:55:53 +0500

    ...

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from app.models import Analysis, UserAnalysis, Medicine, Pharmacy, MedicinePharmacyRel
+from app.models import Analysis, UserAnalysis, Medicine, Pharmacy, MedicinePharmacyRel, Hospital
 
 admin.site.register(Analysis)
 admin.site.register(UserAnalysis)
 admin.site.register(Medicine)
 admin.site.register(Pharmacy)
+admin.site.register(Hospital)
 admin.site.register(MedicinePharmacyRel)
